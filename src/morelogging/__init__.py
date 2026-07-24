c898e79439d509500b2a292e444c0d8ed58d1d35
Bogdan Sandu <github.34373732@gmail.com>
2026-07-24 10:56:15 +0300

    minimal morelogging working version

--- src/morelogging/__init__.py
+++ src/morelogging/__init__.py
@@ -3,7 +3,8 @@ from functools import cached_property
 import hashlib
 import warnings
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Optional
+from collections.abc import Iterable
 from pydantic import BaseModel, field_validator, ValidationInfo
 from enum import Enum
 
@@ -22,6 +23,7 @@ warnings.warn("morelogging is experimental and incomplete. It is not ready to be
 class Log(BaseModel):
     level: LogLevel
     message: str
+    objects: Iterable[Any] = tuple()
 
     @field_validator("message")
     @classmethod
@@ -35,24 +37,41 @@ class Log(BaseModel):
             if len(value) == 0:
                 raise ValueError(f"Log message is an empty string ('{value}')")
 
+        return value
+
+    @field_validator("objects")
+    @classmethod
+    def check_objects(cls, value: Iterable[Any]) -> tuple[Any]:
+        if not value:
+            return tuple()
+        return tuple(value)
+
 class LogHandler(ABC):
     def __init__(self) -> None:
-        self.name = f"<{self.__class__.__name__}> {self.__qualname__}"
-        self.uuid = uuid.uuid4().hex
+        self.__dict__['name'] = f"<{self.__class__.__name__} instance at 0x{hex(id(self))}>"
+        self.__dict__['uuid'] = uuid.uuid4().hex
         self._connect_hook: list[Any] | None = None
         self._position = 0
 
     @cached_property
     def identifier(self) -> str:
-        return hashlib.sha3_512(str((self.name, self.uuid)))
+        return hashlib.sha3_512(str((self.name, self.uuid)).encode('utf8'))
 
     @abstractmethod
-    def format(self, log) -> str:
+    def format(self, log: Log) -> str:
         raise NotImplementedError("LogHandler(ABC).format is an abstract method and not implemented.")
 
     @abstractmethod
-    def commit(self, log) -> None:
+    def commit(self, log: str) -> None:
         raise NotImplementedError("LogHandler(ABC).commit is an abstract method and not implemented.")
+
+    @abstractmethod
+    def open(self) -> None:
+        raise NotImplementedError("LogHandler(ABC).open is an abstract method and not implemented.")
+
+    @abstractmethod
+    def close(self) -> None:
+        raise NotImplementedError("LogHandler(ABC).close is an abstract method and not implemented.")
 
     def update(self) -> None:
         if self._connect_hook is None:
@@ -62,7 +81,7 @@ class LogHandler(ABC):
         while self._position < len(self._connect_hook) - 1:
             self._push(self._connect_hook[self._position])
 
-    def _push(self, log) -> None:
+    def _push(self, log: Log) -> None:
         self.commit(self.format(log))
         self._position += 1
 
@@ -71,13 +90,20 @@ class LogHandler(ABC):
 
     def __setattr__(self, name, value):
         if name in {'name', 'uuid'}:
-            raise RuntimeError(f"Attempted to modify immutable property {LogStream.name} to '{value}'.")
+            raise RuntimeError(f"Attempted to modify immutable property LogHandler.{name} to '{value}'.")
         self.__dict__[name] = value
+
+class SimpleLogHandler(LogHandler):
+    def format(self, log: Log) -> str:
+        return f"{log.level.value} {log.message} {' '.join(map(lambda x: str(x), log.objects))}"
+
+    def commit(self, log: str) -> None:
+        print(log)
 
 class LogStream:
     def __init__(self, name: str) -> None:
-        self.name = name
-        self.uuid = uuid.uuid4().hex
+        self.__dict__["name"] = name
+        self.__dict__["uuid"] = uuid.uuid4().hex
         self.data = []
         self.handlers = {}
 
@@ -100,13 +126,22 @@ class LogStream:
 
     def __setattr__(self, name, value):
         if name in {'name', 'uuid'}:
-            raise RuntimeError(f"Attempted to modify immutable property {LogStream.name} to '{value}'.")
+            raise RuntimeError(f"Attempted to modify immutable property {self.__class__.__name__}.{name} to '{value}'.")
         self.__dict__[name] = value
 
     def _add_item(self, item: list) -> None:
         self.data.append(item)
         for handler in self.handlers.values():
-            handler.push(item)
+            handler._push(item)
 
-    def log(self, level: LogLevel, message: str) -> None:
-        self._add_item([level, message])
+    def log(self, level: LogLevel, message: str, *objects: Optional[Iterable[Any]]) -> None:
+        self._add_item(Log(level=level, message=message, objects=objects or []))
+
+if __name__ == "__main__":
+    if not __debug__:
+        warnings.warn("This is a debug script.")
+
+    stream = LogStream("MyLogger")
+    handler = SimpleLogHandler()
+    stream.add_handler(handler)
+    stream.log(LogLevel.DEBUG, "Test", "abc", None, {'a': 'bc'})
